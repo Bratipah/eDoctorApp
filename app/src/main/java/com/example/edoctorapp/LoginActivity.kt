432467009ddd4a35e8a36483b95f8f85daa5ca78
Thabile-glitch <95622567+Thabile-glitch@users.noreply.github.com>
2021-12-20 20:42:49 +0200

    Update LoginActivity.kt

--- app/src/main/java/com/example/edoctorapp/LoginActivity.kt
+++ app/src/main/java/com/example/edoctorapp/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         setContentView(R.layout.activity_login)
     }
 }
